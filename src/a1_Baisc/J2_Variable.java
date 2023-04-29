77c5021df978994ffda55179641c7f143abe654c
kanhaiya kumar <97101583+Raje-0@users.noreply.github.com>
2023-04-29 09:46:10 +0530

    day1

--- src/a1_Baisc/J2_Variable.java
+++ src/a1_Baisc/J2_Variable.java
@@ -3,26 +3,33 @@ package a1_Baisc;
 public class J2_Variable {
 
 	public static void main(String[] args) {
-		int a,b,c,d,f;   //int take 4 bytes ..>32 bits
-		a=2;
-		b=6;
-		c =a+b;
+
+		byte j = 5;
+
+		char ram = 'a';
+
+		boolean var = true;
+
+		float f1 = 24.5f;
+
+		short m = 5; // short take 2 bytes
+
+		long road = 100000L;
+		long o = 77777777l;
+
+		double x = 6.5;
+		double y = 9.0;
+
+		int a, b, c, d, f; // int take 4 bytes ..>32 bits
+		a = 2;
+		b = 6;
+		c = a + b;
 		System.out.println(c);
-		
-		
-		short m=5;     //short take 2 bytes
-		
-		
-		byte j=5;
-		
-		long o=77777777l;
-		
-		double x=6.5;
-		double y=9.0;
-		
-		char ram='a';
-		
-		String p="kanhaiya";
+
+		c = 10;
+		System.out.println(c);
+
+		String p = "kanhaiya";
 
 	}
 
