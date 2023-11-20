5e103cf0266aaee2d23c852ae415b015e13575b4
kanhaiya kumar <97101583+Raje-0@users.noreply.github.com>
2023-11-20 19:46:15 +0530

    final

---
+++ Hashing/map/M4_TreeMap.java
@@ -0,0 +1,31 @@
+
+package map;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class M4_TreeMap {
+
+	public static void main(String[] args) {
+		// Creating a TreeMap
+		TreeMap<String, Integer> treeMap = new TreeMap<>();
+
+		// Adding elements to the TreeMap
+		treeMap.put("John", 25);
+		treeMap.put("Alice", 30);
+		treeMap.put("Bob", 22);
+		treeMap.put("Eva", 28);
+
+		// Displaying the TreeMap
+		System.out.println("TreeMap: " + treeMap);
+
+		// Accessing elements in TreeMap
+		System.out.println("Age of Alice: " + treeMap.get("Alice"));
+
+		// Iterating through the TreeMap using entrySet()
+		System.out.println("Iterating through TreeMap using entrySet():");
+		for (Map.Entry<String, Integer> entry : treeMap.entrySet()) {
+			System.out.println(entry.getKey() + " - " + entry.getValue());
+		}
+	}
+}
---
+++ InterviewQuestion/ThrowAndThrows.java
@@ -0,0 +1,58 @@
+import java.io.IOException;
+
+/*throw: Used to explicitly throw an exception. In the example, it throws a checked exception 
+ * (IOException) within a method, and the calling method or block needs to handle this exception 
+ * using a try-catch block.
+ * 
+ *  throw keyword is used to throw an exception explicitly.
+* throw keyword is used inside mtd body.
+* we can throw only one exception only one at a time.
+* to define exception
+* only used for single exception
+* it is followed by object
+ */
+public class ThrowAndThrows {
+	// Example of 'throw'
+	static void exampleThrow() {
+		try {
+			throw new IOException("This is a checked exception.");
+		} catch (IOException e) {
+			System.out.println("Caught an exception: " + e.getMessage());
+		}
+	}
+
+	/*
+	 * throws: Used in the method signature to declare that a method may throw one
+	 * or more types of exceptions. It doesn't throw an exception itself but signals
+	 * that the method might propagate an exception of the specified type. The
+	 * calling method or block is responsible for handling the declared
+	 * exception(s).
+	 * 
+	 * * throw keyword is used to declare an exception. throw keyword is used with
+	 * method signature. we can handle multiple exception using throw keyword.. only
+	 * used for multiple exception it is followed by class(mtd) write beside method.
+	 */
+	// Example of 'throws'
+	static void exampleThrows() throws RuntimeException {
+		throw new RuntimeException("This is an unchecked exception.");
+	}
+
+	public static void main(String[] args) {
+		// Example of 'throw'
+		exampleThrow();
+
+		// Example of 'throws'
+		try {
+			exampleThrows();
+		} catch (RuntimeException e) {
+			System.out.println("Caught an exception: " + e.getMessage());
+		}
+	}
+}
+/*
+ * In the main method, exampleThrow demonstrates the use of throw by throwing a
+ * checked exception, and exampleThrows shows the use of throws to declare that
+ * it may throw an unchecked exception.
+ * 
+ * 
+ */
